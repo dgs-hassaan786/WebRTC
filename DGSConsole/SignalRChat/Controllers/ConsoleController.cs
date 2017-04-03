0d06d42c3e045a42aa87f566bbcdc9fa77d64a37
hassaan.khan@dgsworld.com <hassaan09>
2017-04-03 15:01:45 +0500

    Api methods for get all users and method checked in

---
+++ DGSConsole/SignalRChat/Controllers/ConsoleController.cs
@@ -0,0 +1,30 @@
+﻿using DGSConsole.Agent.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DGSConsole.Agent.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class ConsoleController : ApiController
+    {
+        [Route("/users")]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var users = AgentDataProvider.Cache.Select(x=>x.Value).ToList();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+    }
+}
